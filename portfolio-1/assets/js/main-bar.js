9437c2b46bbda4ca42ddbcec40cb7d2c767483ca
HuangWen8078 <92803805+HuangWen8078@users.noreply.github.com>
2022-06-29 23:41:44 +0800

    Update main-bar.js

--- portfolio-1/assets/js/main-bar.js
+++ portfolio-1/assets/js/main-bar.js
@@ -228,7 +228,7 @@
     
     
     
-    d3.csv('../D3.js/bar_chart/movies.csv',type).then(
+    d3.csv('/portfolio-1/D3.js/bar_chart/movies.csv',type).then(
         res => {
             ready(res);
             // console.log(res);
