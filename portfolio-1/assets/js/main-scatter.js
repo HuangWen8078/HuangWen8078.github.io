4f9cec0430d64bf4bb1425f6242bd7a8ba8f0aca
HuangWen8078 <92803805+HuangWen8078@users.noreply.github.com>
2022-06-29 23:52:51 +0800

    Update main-scatter.js

--- portfolio-1/assets/js/main-scatter.js
+++ portfolio-1/assets/js/main-scatter.js
@@ -230,7 +230,7 @@ function ready(movies) {
 }
 
 //Load Data
-d3.csv('portfolio-1/D3.js/scatter_chart/movies.csv', type).then(
+d3.csv('/portfolio-1/D3.js/scatter_chart/movies.csv', type).then(
     res => {
         ready(res);
         // console.log(res);
